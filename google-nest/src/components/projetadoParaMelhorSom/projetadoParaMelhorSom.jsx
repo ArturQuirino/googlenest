52aa2bad6f59fe332708b04969307c754cbb02e9
Artur Quirino <artur.pquirino@gmail.com>
2020-09-21 11:19:18 -0300

    Adição do corte da imagem no rolamento

--- google-nest/src/components/projetadoParaMelhorSom/projetadoParaMelhorSom.jsx
+++ google-nest/src/components/projetadoParaMelhorSom/projetadoParaMelhorSom.jsx
@@ -13,6 +13,11 @@ function mudarOpacidadeNoScroll(classeElemento) {
     } else {
       element.style.opacity =
         (window.innerHeight - rect.top) / window.innerHeight;
+
+      document.documentElement.style.setProperty(
+        '--db-title-y',
+        ((window.innerHeight - rect.top) * 100) / window.innerHeight + 'px'
+      );
     }
   }
 
@@ -20,18 +25,62 @@ function mudarOpacidadeNoScroll(classeElemento) {
   window.addEventListener('scroll', mudarOpacidade);
 }
 
+function mudarCorteNoScroll(classeElemento) {
+  var element = document.querySelector(classeElemento);
+
+  function mudarcorte() {
+    const rect = element.getBoundingClientRect();
+    if (rect.top < 0) {
+      document.documentElement.style.setProperty('--clip1', '0%');
+    } else if (rect.top > window.innerHeight) {
+      document.documentElement.style.setProperty('--clip1', '100%');
+    } else {
+      element.style.opacity =
+        (window.innerHeight - rect.top) / window.innerHeight;
+
+      document.documentElement.style.setProperty(
+        '--clip1',
+        100 - ((window.innerHeight - rect.top) * 100) / window.innerHeight + '%'
+      );
+    }
+  }
+
+  window.addEventListener('load', mudarcorte);
+  window.addEventListener('scroll', mudarcorte);
+}
+
 class ProjetadoParaMelhorSom extends Component {
   componentDidMount() {
     mudarOpacidadeNoScroll('.projetado-para-melhorar-som__titulo');
+    mudarCorteNoScroll('.projetado-para-melhorar-som__container2-imagem2');
   }
   render() {
     return (
       <div className="projetado-para-melhorar-som">
-        <span className="projetado-para-melhorar-som__titulo">
-          Projetado para ter o melhor som.
-          <br />
-          Design que combina com sua casa.
-        </span>
+        <div className="projetado-para-melhorar-som__container-todo-titulo">
+          <div className="projetado-para-melhorar-som__titulo">
+            <span>
+              Projetado para ter o melhor som.
+              <br />
+              Design que combina com sua casa.
+            </span>
+          </div>
+        </div>
+        <div className="projetado-para-melhorar-som__container-todas-imagens">
+          <div className="projetado-para-melhorar-som__container1-imagem">
+            <div className="projetado-para-melhorar-som__container2-imagem">
+              <div className="projetado-para-melhorar-som__container-imagem">
+                <div className="projetado-para-melhorar-som__imagem-externa"></div>
+              </div>
+            </div>
+
+            <div className="projetado-para-melhorar-som__container2-imagem2">
+              <div className="projetado-para-melhorar-som__container-imagem">
+                <div className="projetado-para-melhorar-som__imagem-interna"></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
