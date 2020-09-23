5b105bb84a70d1a026ba1241fc3623d8c0f0cd94
Artur Quirino <artur.pquirino@gmail.com>
2020-09-23 16:50:49 -0300

    Adição de efeitos simulando as luzes e a televisão

--- google-nest/src/components/comandoDeVoz/comandoDeVoz.jsx
+++ google-nest/src/components/comandoDeVoz/comandoDeVoz.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import './comandoDeVoz.css';
 
+function mudarOpacidadeNoScroll() {
+  const element = document.querySelector('.comando-de-voz');
+
+  function mudarOpacidade() {
+    const rect = element.getBoundingClientRect();
+    if (rect.bottom <= 1.2 * window.innerHeight) {
+      element.classList.add('in-viewport');
+    } else {
+      element.classList.remove('in-viewport');
+    }
+  }
+
+  window.addEventListener('load', mudarOpacidade);
+  window.addEventListener('scroll', mudarOpacidade);
+}
+
 class ComandoDeVoz extends Component {
+  componentDidMount() {
+    mudarOpacidadeNoScroll();
+  }
   render() {
     return (
       <section className="comando-de-voz">
         <div className="comando-de-voz__tv-ligada"></div>
         <div className="comando-de-voz__tv-desligada"></div>
-        <div>Teste</div>
+        <div></div>
       </section>
     );
   }
